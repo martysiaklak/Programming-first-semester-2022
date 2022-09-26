72e0e42b6a9457aa7dbbe24e3e7465915c61d775
Marta Klakovych <mklakovych08@gmail.com>
2022-09-26 22:56:45 +0300

    add 26/09 homework

---
+++ home work 26.09 radius/home work 26.09 radius/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include<string>
+#include<math.h>
+# define M_PI           3.14159265358979323846
+using namespace std;
+int main()
+{
+	double radius;
+	double C;
+	double S;
+	double V;
+	//asking for entering a radius
+	cout << "Enter a radius:";
+	//remembering a variable
+	cin >> radius;
+	//counting a circumference
+	C = 2 * radius * M_PI;
+	//printing it
+	cout << "Circumference="<<C<<endl;
+	//calculating an area of a circle
+	S = M_PI * pow(radius,2);
+	//printing it
+	cout << "Area of a circle=" << S << endl;
+	//calculating a volume of a sphere   is V = 4/3 πr³.
+	V= 4/3* M_PI * pow(radius, 3);
+	//printing it
+	cout << "Volume of a sphere=" << V << endl;
+
+		return 0;
+}
+
+
+
