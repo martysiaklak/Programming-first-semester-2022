16a5ad4f1bdea70dab9686804f8d7c385396a2c7
Marta Klakovych <mklakovych08@gmail.com>
2022-11-01 23:27:09 +0200

    adding  new project

---
+++ lcm/lcm/Source.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, lcm;
+    cout << "Enter the first number" << endl;
+    cin >> a;
+    cout << "Enter the second number" << endl;
+    cin >> b;
+    cout << "Enter the third number" << endl;
+    cin >> c;
+    cout << "Enter the fourth number" << endl;
+    cin >> d;
+
+    int max(int x, int y);
+
+
+    int left_max = max(a, b);
+    int right_max = max(c, d);
+    int final_max = max(left_max, right_max);
+    cout << final_max;
+   
+    lcm = final_max;
+    while (1)
+    {
+        if (lcm % a == 0 && lcm % b == 0 && lcm % c == 0 && lcm % d == 0)
+        {
+            cout << "lcm is: " << lcm;
+            break;
+        }
+        lcm++;
+    }
+
+    return 0;
+}
+int max(int x, int y)
+{
+    if (x > y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+
+}
