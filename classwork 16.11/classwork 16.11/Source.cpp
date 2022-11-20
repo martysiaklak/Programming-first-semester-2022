e6244133b2742e95f300a00cfd0a370dc4ceaa7a
Marta Klakovych <mklakovych08@gmail.com>
2022-11-20 22:24:50 +0200

    add classwork

--- classwork 16.11/classwork 16.11/Source.cpp
+++ classwork 16.11/classwork 16.11/Source.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
 using namespace std;
-/*enum dogbreed
+enum dogbreed
 {
 	HASKI,
 	BULDOG,
 	TAKSA,
-	CHIHUAHUA
+	CHIHUAHUA,
+	NOBREED
 
 };
-void add_el(dogbreed * array, int size); //чому через вказівник?
+void add_el(dogbreed * array, int size); 
+dogbreed str_to_dogbreed(string name);
 int main()
 {
 	cout << "Enter size:" << endl;
 	int size;
-	cin >> size;
+	cin >> size; 
 	dogbreed* array_breed = new dogbreed[size];
 	add_el(array_breed, size);
+	bool finish = false;
+	while (finish == false)
+	{
+		string input;
+		cout << "Enter a breed: " << endl;
+		cin >> input;
+		dogbreed result = str_to_dogbreed(input);
+		for (int i = 0; i < size; i++)
+		{
+			if (result == array_breed[i])
+			{
+				cout << "Breed found" << endl;
+				finish = true;
+				break;
+			}
+		}
+		if (finish==false)
+		{
+			cout << "Breead isn't found" << endl;
+		}
+	}
 	return 0;
 }
 void add_el(dogbreed* array, int size)
@@ -48,4 +71,31 @@ void add_el(dogbreed* array, int size)
 
 			}
 	}
-}*/
+}
+dogbreed str_to_dogbreed(string name)
+{
+	for (int i = 0; i < name.size(); ++i) {
+		name[i] = tolower(name[i]);
+	}
+	if (name == "haski")
+	{
+		return HASKI;
+	}
+	else if (name == "buldog")
+	{
+		return BULDOG;
+	}
+	else if (name == "taksa")
+	{
+		return TAKSA;
+	}
+	else if (name == "chihuahua")
+	{
+		return CHIHUAHUA;
+	}
+	else
+	{
+		return NOBREED;
+	}
+
+}
