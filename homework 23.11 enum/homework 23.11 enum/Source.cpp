437ec0a09fffea75d80802b0fe03d502339e4212
Marta Klakovych <mklakovych08@gmail.com>
2022-11-22 00:38:00 +0200

    fixed 1

--- homework 23.11 enum/homework 23.11 enum/Source.cpp
+++ homework 23.11 enum/homework 23.11 enum/Source.cpp
@@ -17,14 +17,13 @@ int quantity(days* array, int size, days MONDAY);
 string from_days(days MONDAY);
 int main()
 {
-	//string day;
 	int size;
-	/*cout << "Enter a day:" << endl;
-	cin >> day;*/
 	cout << "Enter size:" << endl;
 	cin >> size;
 	string* array_days = new string[size];
 	days* day_of_week = new days[size];
+	days already_printed[7];
+	int already_printed_index = 0;
 	for (int i = 0; i < size; i++)
 	{
 		cout << "Enter " << i + 1 << ": " << endl;
@@ -32,17 +31,35 @@ int main()
 		day_of_week[i] = from_string(array_days[i]);
 
 	}
-	sort(day_of_week, day_of_week + size);
+	sort(day_of_week, day_of_week + size); //sorting
+	cout << "Sorted list:" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout<<from_days(day_of_week[i])<<endl;
+	}
 	for (int i = 0; i < size; i++)
 	{
 		int sum=quantity(day_of_week, size, day_of_week[i]);
-		string output = from_days(day_of_week[i]);
-		cout << output << "-->" << sum << "times" << endl;
+		string output = from_days(day_of_week[i]);  
+		bool is_in_array = false;
+		for (int k = 0; k < already_printed_index; k++) //missing printing those times, when a day repeats
+		{
+			if (day_of_week[i] == already_printed[k])
+			{
+				is_in_array = true;
+				break;
+			}
+		}
+		if (is_in_array == false)
+		{
+			cout << output << "-->" << sum << "times" << endl;
+			already_printed[already_printed_index] = day_of_week[i];
+			already_printed_index++;
+		}
 	}
-
 	return 0;
 }
-days from_string(string input)
+days from_string(string input)   //importing string to days type
 {
 	for (int i = 0; i < input.size(); i++)
 	{
@@ -82,43 +99,43 @@ days from_string(string input)
 	}
 
 }
-string from_days(days MONDAY)
+string from_days(days MONDAY)    //importing days type to string
 {
 	if (MONDAY == 0)
 	{
-		return "MONDAY";
+		return "monday";
 	}
 	if (MONDAY == 1)
 	{
-		return "TUESDAY";
+		return "tuesday";
 	}
 	if (MONDAY == 2)
 	{
-		return 	"WEDNESDAY";
+		return 	"wednesday";
 	}
 	if (MONDAY == 3)
 	{
-		return 	"THURSDAY";
+		return 	"thursday";
 	}
 	if (MONDAY == 4)
 	{
-		return "FRIDAY";
+		return "friday";
 	}
 	if (MONDAY == 5)
 	{
-		return "SATURDAY";
+		return "saturday";
 	}
 	if (MONDAY == 6)
 	{
-		return "SUNDAY";
+		return "sunday";
 	}
 	else
 	{
-		return "NOTHING";
+		return "nothing";
 	}
 
 }
-int quantity(days* array, int size, days MONDAY)
+int quantity(days* array, int size, days MONDAY)   //counting repeatings
 {
 	int sum = 0;
 	for (int i = 0; i < size; i++)
