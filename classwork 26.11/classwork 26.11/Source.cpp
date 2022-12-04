b86844100aa93841963f35c5b9e94f2cd8f6801d
Marta Klakovych <mklakovych08@gmail.com>
2022-12-04 17:20:10 +0200

    add new functions

--- classwork 26.11/classwork 26.11/Source.cpp
+++ classwork 26.11/classwork 26.11/Source.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
+# define M_PI   3.14159265358979323846 
 using namespace std;
 struct Point {
 	int x;
 	int y;
 	friend istream& operator>>(istream& is, Point& point)
 	{
-		//cout << "Enter x" << endl;
+		cout << "Enter x" << endl;
 		is >> point.x;
-		//cout << "Enter y" << endl;
+		cout << "Enter y" << endl;
 		is >> point.y;
 		return is;
 	}
@@ -29,9 +31,9 @@ public:
 	Circle(const Circle& circle) : center(circle.center), radius(circle.radius) {}
 	friend istream & operator>>(istream & is, Circle & circle)
 	{
-		//cout << "Enter radius" << endl;
-		is >> circle.radius;
-		//cout << "Enter center" << endl;
+		cout << "Enter radius" << endl;
+		is >> circle.radius;                    
+		cout << "Enter center" << endl;
 		is >> circle.center;
 		return is;
 	}
@@ -42,19 +44,88 @@ public:
 		os <<  circle.center << endl;
 		return os;
 	}
+	bool operator<(const Circle& circle2)
+	{
+		return S() < circle2.S();
+	}
+	bool operator>(const Circle& circle2)
+	{
+		return S() > circle2.S();
+	}
+	double S()  const       //calculating square
+	{
+		double S1 = M_PI * pow(radius, 2);
+		return S1;
+	}
+	double C()
+	{
+		double C = M_PI * 2 * radius;
+		return C;
+	}
+	bool intersecting(const Circle& circle2)
+	{
+		double d = sqrt((center.x - circle2.center.x) * (center.x - circle2.center.x) + (center.y - circle2.center.y) * (center.y - circle2.center.y));
+		if (d < abs(radius - circle2.radius))
+		{
+			return false;
+		}
+		if (d <= radius + circle2.radius)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
 };
+void sort(Circle* array);
 int main()
 {
+	Circle array[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Data about circle " << i<<endl;
+		cin >> array[i];
+	}
+	sort(array);
+	for (int i = 0; i < 10; i++)
+	{
+		cout << array[i].S()<<endl;
+	}
+	cout << "Enter circle" << endl;
 	Circle circle;
-	//cout << "¬вед≥ть дан≥ про коло" << endl;
-	//cin >> circle; //operator >> (cin, circle)
-	//cout << circle;
-	ifstream ifs;
+	cin >> circle;
+	int sum = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		if (array[i].intersecting(circle) == true)  
+		{
+			sum += 1;
+		}
+	}
+	cout << "sum= "<<sum;
+	/*ifstream ifs;
 	ofstream ofs("result.txt");
 	ifs.open("data.txt");
 	ifs >> circle;
 	ifs.close();
 	ofs << circle;
-	ofs.close();
+	ofs.close();*/
 	return 0;
+}
+void sort(Circle* array)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i + 1; j < 10; j++)
+		{
+			if (array[i] < array[j])
+			{
+				Circle temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
 }
---
+++ classwork 30.11/classwork 30.11/Source.cpp
@@ -0,0 +1,121 @@
+#include <iostream> 
+using namespace std;
+struct Point
+{
+	float x, y;
+
+	friend ostream& operator<<(ostream& os, Point point)
+	{
+		os << "x: " << point.x << endl;
+		os << "y: " << point.y << endl;
+
+
+			//якщо коеф від'ємний, то не + а -. через if
+		/*os << "a= " << line_.a << endl;
+		os << "b= " << line_.b << endl;
+		os << "c= " << line_.c << endl;*/
+		return os;
+	}
+};
+class line {
+private:
+	float a, b, c;
+public:
+	line() : a(), b(), c() {}
+	line(const line& line) : a(line.a), b(line.b), c(line.c) {}
+	friend istream& operator>>(istream& is, line& line_)
+	{
+		cout << "Enter a" << endl;
+		is >> line_.a;
+		cout << "Enter b" << endl;
+		is >> line_.b;
+		cout << "Enter c" << endl;
+		is >> line_.c;
+		return is;
+	}
+	friend ostream& operator<<(ostream& os, line line_)
+	{
+		os << line_.a << 'x' << '+' << line_.b << 'y' << '+' << line_.c << "=0";
+		//якщо коеф від'ємний, то не + а -. через if
+		/*os << "a= " << line_.a << endl;
+		os << "b= " << line_.b << endl;
+		os << "c= " << line_.c << endl;*/
+		return os;
+	}
+	Point getInterception(line &line_)
+	{
+		float bottom = a * line_.b - b * line_.a;
+		
+		float x = (b * line_.c - line_.b * c) / bottom;
+		float y = (c * line_.a - line_.c * a) / bottom;
+		Point p;
+		p.x = x;
+		p.y = y;
+		return p;
+	}
+
+	bool isIntercepting(line& line_)
+	{
+		float bottom = a * line_.b - b * line_.a;
+
+		if (bottom == 0)
+		{
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	float getAngle(line & line2) 
+	{
+		double angle = (line2.a * a + line2.b * b) / (sqrt(line2.a * line2.a + line2.b * line2.b) * sqrt(a * a + b * b);
+		return angle;
+	}
+	bool isonline(Point& line3)
+	{
+		if (a* line3.x + b * line3.y + c == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
+	int main()
+	{
+		line line[10];
+		for (int i = 0; i < 10; i++)
+		{
+			cin >> line[i];
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = i+1; j< 9; j++)
+			{
+				if (line[i].isIntercepting(line[j]) == true)
+				{
+					cout << "line " <<line[i]<<"peretenaitsa z " <<line[j] << endl;
+					cout << "point of interseption is " << line[i].getInterception(line[j]) << endl;
+				}
+			}
+		}
+
+		/*line line, line;
+		cout << "Data about line" << endl;
+		operator >> (cin, line);
+		cout << "Data about line2" << endl;
+		operator >> (cin, line2);
+		cout << line;
+		cout << line2;
+		if (line.isIntercepting(line2) == false)
+		{
+			cout << "Paralelni" << endl;
+		}
+		else
+		{
+			cout << line.getInterception(line2).x<< line.getInterception(line2).y;
+		}
+		return 0;*/
+	}
