77f9f2166810156942114aa38db9a9391069f744
Marta Klakovych <mklakovych08@gmail.com>
2023-03-29 18:09:50 +0300

    added comments

--- template.sort/template.sort/Source.cpp
+++ template.sort/template.sort/Source.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 template<typename T>
-T sort(T* array, int size)
+T sort(T* array, int size)    //bubble sort
 {
   int i, j;
   for (i = 0; i < size - 1; i++)
@@ -42,25 +42,25 @@ public:
 };
 int main()
 {
-    int int_arr[] = { 5, 2, 9, 1, 5 };
+    int int_arr[] = { 5, 2, 9, 1, 5 };    //int array
     sort(int_arr, 5);
     cout << "sorted int array:" << endl;
     for (int i = 0; i < 5; i++) 
     {
         cout <<i<<": "<< int_arr[i] << endl;
     }
-   
-    string string_arr[] = { "apple", "dog", "cat", "banana" };
+    cout << "------------------" << endl;
+    string string_arr[] = { "apple", "dog", "cat", "banana" }; //string array
     sort(string_arr, 4);
     cout << "sorted string array:" << endl;
     for (int i = 0; i < 4; i++) 
     {
         cout << i << ": "<< string_arr[i] << endl;
     }
-
-    Person person_arr[]={Person("Bob", "456 Oak Ave."),Person("Charlie", "789 Elm St."), Person("Alice", "123 Main St.")};
+    cout <<"------------------" << endl;
+    Person person_arr[]={Person("Bob", "456 Oak Ave."),Person("Charlie", "789 Elm St."), Person("Alice", "123 Main St.")}; //Person array
     sort(person_arr, 3);
-    cout << "sorted person array:" << endl;
+    cout << "sorted Person array(by name):" << endl;
     for (int i = 0; i < 3; i++)
     {
         cout << i << ": " << person_arr[i] << endl;
