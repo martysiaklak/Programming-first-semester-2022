0695c4b7b9efb5674c410210b733b2d891bbc0e5
Marta Klakovych <mklakovych08@gmail.com>
2023-05-09 22:19:15 +0300

    needs comments

---
+++ bits/bits/Source.cpp
@@ -0,0 +1,134 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <iostream>
+#include "doctest.h"
+using namespace std;
+
+class Set
+{
+private:
+	char a[5];
+public:
+	Set()
+	{
+		for (size_t i = 0; i < 5; i++)
+		{
+			a[i] = 0;
+		}
+	}
+	void insert(char elem)
+	{
+		int index;
+		if (elem >= 'a' && elem <= 'z')
+			index = elem - 'a';
+		else
+			index = elem + 26 - '0';
+		a[index / 8] |= (1 << (7 - index % 8));
+	}
+	void remove(char elem)
+	{
+		int index;
+		if (elem >= 'a' && elem <= 'z')
+			index = elem - 'a';
+		else
+			index = elem + 26 - '0';
+		a[index / 8] &= ~(1 << (7 - index % 8));
+	}
+	bool contains(char elem)
+	{
+		int index;
+		if (elem >= 'a' && elem <= 'z')
+			index = elem - 'a';
+		else
+			index = elem + 26 - '0';
+		return a[index / 8] & (1 << (7 - index % 8));
+	}
+	void intersect(Set other)
+	{
+		for (size_t i = 0; i < 5; i++)
+		{
+			a[i] &= other.a[i];
+		}
+	}
+	void unionWith(Set other)
+	{
+		for (size_t i = 0; i < 5; i++)
+		{
+			a[i] |= other.a[i];
+		}
+	}
+};
+
+TEST_CASE("Insert and Contains")
+{
+	Set set;
+	set.insert('a');
+	set.insert('b');
+	set.insert('1');
+	CHECK(set.contains('a') == true);
+	CHECK(set.contains('b') == true);
+	CHECK(set.contains('1') == true);
+	CHECK(set.contains('c') == false);
+}
+
+TEST_CASE("Insert and Remove")
+{
+	Set set;
+	set.insert('a');
+	set.insert('b');
+	set.remove('a');
+	set.remove('c');
+	CHECK(set.contains('a') == false);
+	CHECK(set.contains('b') == true);
+	CHECK(set.contains('c') == false);
+}
+
+TEST_CASE("Insert and Remove Multiple Times")
+{
+	Set set;
+	set.insert('a');
+	set.insert('b');
+	set.remove('a');
+	set.remove('b');
+	set.insert('1');
+	set.insert('2');
+	set.remove('1');
+	CHECK(set.contains('a') == false);
+	CHECK(set.contains('b') == false);
+	CHECK(set.contains('1') == false);
+	CHECK(set.contains('2') == true);
+}
+TEST_CASE("Intersection")
+{
+	Set set1;
+	set1.insert('a');
+	set1.insert('b');
+	set1.insert('1');
+
+	Set set2;
+	set2.insert('b');
+	set2.insert('c');
+
+	set1.intersect(set2);
+	CHECK(set1.contains('a') == false);
+	CHECK(set1.contains('b') == true);
+	CHECK(set1.contains('1') == false);
+	CHECK(set1.contains('c') == false);
+}
+TEST_CASE("Union")
+{
+	Set set1;
+	set1.insert('a');
+	set1.insert('b');
+	set1.insert('1');
+
+	Set set2;
+	set2.insert('b');
+	set2.insert('c');
+
+	set1.unionWith(set2);
+	CHECK(set1.contains('a') == true);
+	CHECK(set1.contains('b') == true);
+	CHECK(set1.contains('1') == true);
+	CHECK(set1.contains('c') == true);
+}
+
